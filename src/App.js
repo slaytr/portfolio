d623c50fb8470a864eb588ab6749e2df674a94f9
Bill Li <slaytraz@gmail.com>
2019-09-20 23:26:54 +1200

    intial site layout

--- src/App.js
+++ src/App.js
@@ -2,25 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  render() {
+    return (
+      <div className="content">
+        <div className="brand">
+          <span id="text-brand">Bill</span>
+        </div>
+        <div className="home">
+          <h1>My Portfolio</h1>
+          <p>
+            A collection of my work
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
